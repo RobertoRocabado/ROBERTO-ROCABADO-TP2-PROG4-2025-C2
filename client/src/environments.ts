b6d00c299cf8e1f17557f09eeab0cec41512ecde
roberto rocabado <robertorocabado17@gmail.com>
2025-11-05 18:04:25 -0300

    cambios para vercel

--- client/src/environments.ts
+++ client/src/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiBase: 'http://localhost:3000',
-  apiBaseUrl: 'https://TU_SERVICE.onrender.com',
+  apiBaseUrl: 'https://roberto-rocabado-tp-2-prog-4-2025-c.vercel.app/',
   apiPrefix: ''   // <- sin /api
 };
