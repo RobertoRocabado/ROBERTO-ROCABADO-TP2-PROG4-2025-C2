93ef873198db5cbda9e891e7ba44601400917667
roberto rocabado <robertorocabado17@gmail.com>
2025-11-05 22:08:46 -0300

    cambio para ver deploy

--- client/src/environments.ts
+++ client/src/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBase: 'http://localhost:3000',
-  // apiBaseUrl: 'https://roberto-rocabado-tp-2-prog-4-2025-c.vercel.app/',
+  // apiBase: 'http://localhost:3000',
+  apiBase: 'https://roberto-rocabado-tp2-prog4-2025-c2.onrender.com/',
   apiPrefix: ''   // <- sin /api
 };
