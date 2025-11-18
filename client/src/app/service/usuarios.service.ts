39c3c40b52190f92d5cd5a80440493ceffab09b2
roberto rocabado <robertorocabado17@gmail.com>
2025-11-18 14:32:39 -0300

    cambios para el front

--- client/src/app/service/usuarios.service.ts
+++ client/src/app/service/usuarios.service.ts
@@ -1,35 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Usuario } from './auth.service';
-
-// @Injectable({ providedIn: 'root' })
-// export class UsuariosService {
-//   constructor(private http: HttpClient) {}
-
-//   findAll() {
-//     return this.http.get<Usuario[]>(`/usuarios`, { withCredentials: true });
-//   }
-
-//   create(dto: Partial<Usuario> & { password: string }) {
-//     return this.http.post<Usuario>(`/usuarios`, dto, { withCredentials: true });
-//   }
-
-//   update(id: string, dto: Partial<Usuario> & { password?: string }) {
-//     return this.http.patch<Usuario>(`/usuarios/${id}`, dto, { withCredentials: true });
-//   }
-
-//   deshabilitar(id: string) {
-//     return this.http.delete<Usuario>(`/usuarios/${id}`, { withCredentials: true });
-//   }
-
-//   habilitar(id: string) {
-//     return this.http.post<Usuario>(`/usuarios/${id}/habilitar`, {}, { withCredentials: true });
-//   }
-// }
+import { environment } from '../../environments';
 
 @Injectable({ providedIn: 'root' })
 export class UsuariosService {
-  private api = 'http://localhost:3000'; // puerto donde corre Nest
+  // private api = 'environment.apiBase}'; 
+  private api = `${environment.apiBase}`;
 
   constructor(private http: HttpClient) {}
 
